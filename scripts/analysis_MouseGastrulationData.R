77cdbf6ecde69bed78fb5aa1d024d7570c9b2eb3
jingkui.wang <jingkui.wang@imp.ac.at>
2023-09-06 16:09:46 +0200

    save cca and rpca integration

--- scripts/analysis_MouseGastrulationData.R
+++ scripts/analysis_MouseGastrulationData.R
@@ -47,11 +47,10 @@ library(future)
 library(data.table)
 library(tidyverse)
 
-options(future.globals.maxSize = 120000 * 1024^2)
+options(future.globals.maxSize = 160000 * 1024^2)
 mem_used()
 
 species = '_mouseGastrulation'
-
 
 
 ##########################################
@@ -214,6 +213,24 @@ ggsave(filename = paste0(resDir, '/Featureplots_Pax6_Foxa2_blended_ordered_subse
 ##########################################
 # test Seurat data integration
 ##########################################
+srat = readRDS(file = paste0(RdataDir,  'seuratObject_EmbryoAtlasData_all36sample_RNAassay.rds'))
+xx = readRDS(file = paste0('../results/dataset_scRNAseq_MouseGastrulationData/Rdata/',
+                           'seuratObject_EmbryoAtlasData_all36sample.rds'))
+
+umap.embedding = xx@reductions$umap@cell.embeddings
+umap.embedding = umap.embedding[match(colnames(srat), rownames(umap.embedding)), ]
+srat[['umap']] = Seurat::CreateDimReducObject(embeddings=umap.embedding,
+                                              key='UMAP_',
+                                              assay='RNA')
+rm(xx)
+rm(umap.embedding)
+
+## filter unlikely celltypes in the reference
+sels = grep('Erythroid|Blood|Allantois|mesoderm|Haemato|Cardiomy|Endothelium|Mesenchyme', srat$celltype, 
+            invert = TRUE)
+srat = subset(srat, cells = colnames(srat)[sels])
+
+
 aa =  readRDS(file = paste0('../results/Rdata/',  
                             'seuratObject_merged_cellFiltered_doublet.rm_mt.ribo.geneFiltered_regressout.nCounts_',
                             'cellCycleScoring_annot.v1_', 'mNT_scRNAseq',
@@ -224,13 +241,22 @@ levels_sels = c("day2_beforeRA",
                 "day2.5_RA", "day3_RA.rep1", "day3.5_RA",   "day4_RA", "day5_RA",
                 "day2.5_noRA", "day3_noRA",  "day3.5_noRA", "day4_noRA", "day5_noRA")
 
-levels_sels = c("day2_beforeRA",  "day2.5_RA", "day3_RA.rep1", "day3.5_RA")
+levels_sels = c("day2_beforeRA",  "day2.5_RA", "day3_RA.rep1", "day3.5_RA",
+                "day4_RA", "day5_RA", "day6_RA")
+
+data_version = "subsettingRef_mNT.RA.d2_d6_Harmony"
+outDir = paste0(resDir, 'dataMapping_', data_version)
+system(paste0('mkdir -p ', outDir))
 
 Idents(aa) = factor(aa$condition)
 aa = subset(aa, idents = levels_sels)
 
+aa = subset(x = aa, downsample = 2000)
+
 aa$dataset = 'mNT'
+aa$stage = aa$condition
 srat$dataset = 'ref'
+
 
 #refs <- CreateAssayObject(counts = srat@assays[["originalexp"]]@counts)
 #metadata = srat@meta.data
@@ -242,77 +268,140 @@ features.common = intersect(rownames(aa), rownames(srat))
 
 refs.merged = merge(aa, y = srat, add.cell.ids = c("mNT", "mouseGastrulation"), project = "RA_competence")
 
+Run_Harmony = FALSE
+if(!Run_Harmony){
+  refs.merged = NormalizeData(refs.merged, normalization.method = "LogNormalize")
+  refs.merged <- FindVariableFeatures(refs.merged, selection.method = "vst")
+  refs.merged <- ScaleData(refs.merged, verbose = TRUE)
+  refs.merged <- RunPCA(refs.merged, verbose = TRUE)
+  
+  ## original code 
+  # from http://htmlpreview.github.io/?https://github.com/immunogenomics/harmony/blob/master/docs/advanced.html
+  #V <- harmony::cell_lines$scaled_pcs
+  #V_cos <- cosine_normalize(V, 1)
+  #meta_data <- harmony::cell_lines$meta_data  
+  V = refs.merged@reductions$pca@cell.embeddings
+  meta_data = refs.merged@meta.data
+  
+  set.seed(1)
+  harmony_embeddings <- harmony::HarmonyMatrix(
+    data_mat = V, ## PCA embedding matrix of cells
+    meta_data = meta_data, ## dataframe with cell labels
+    #vars_use = 'dataset',
+    theta = 0.5, ## cluster diversity enforcement
+    vars_use = 'dataset', ## variable to integrate out
+    npcs = 30,
+    nclust = 5, ## number of clusters in Harmony model
+    max.iter.harmony = 10, ## stop after initialization
+    return_object = FALSE, ## return the full Harmony model object
+    do_pca = FALSE ## don't recompute PCs
+    
+  )
+  
+  p1 <- do_scatter(harmony_embeddings, meta_data, 'dataset') + 
+    labs(title = 'Colored by dataset')
+  p2 <- do_scatter(harmony_embeddings, meta_data, 'cell_type') + 
+    labs(title = 'Colored by cell type')
+  cowplot::plot_grid(p1, p2, nrow = 1)
+  
+  
+  refs.merged[['harmony']] = Seurat::CreateDimReducObject(embeddings=harmony_embeddings,
+                                                key='HARMONY_',
+                                                assay='RNA')
+  
+  ref.combined <- RunUMAP(refs.merged, reduction = "pca", dims = 1:50, n.neighbors = 50, 
+                          min.dist = 0.1) 
+  
+  
+}
+
+
 ref.list <- SplitObject(refs.merged, split.by = "dataset")
 
-rm(list = c('refs.merged')) # remove big seurat objects to clear memory
+rm(list = c('refs.merged', 'aa', 'srat')) # remove big seurat objects to clear memory
 
 # normalize and identify variable features for each dataset independently
 ref.list <- lapply(X = ref.list, FUN = function(x) {
   x <- NormalizeData(x, normalization.method = "LogNormalize")
-  x <- FindVariableFeatures(x, selection.method = "vst", nfeatures = 3000)
+  x <- FindVariableFeatures(x, selection.method = "vst")
   
 })
 
 # select features that are repeatedly variable across datasets for integration run PCA on each
 # dataset using these features
-features <- SelectIntegrationFeatures(object.list = ref.list, nfeatures = 3000)
+features <- SelectIntegrationFeatures(object.list = ref.list)
 
 ref.list <- lapply(X = ref.list, FUN = function(x) {
-  x <- ScaleData(x, features = features.common, verbose = FALSE)
-  x <- RunPCA(x, features = features, verbose = FALSE)
+  x <- ScaleData(x, features = features.common, verbose = TRUE)
+  x <- RunPCA(x, features = features, verbose = TRUE)
   
 })
 
-
 ref.anchors <- FindIntegrationAnchors(object.list = ref.list, 
                                       anchor.features = features, 
-                                      reduction = "rpca", 
-                                      k.anchor = 5)
+                                      reference = c(2),
+                                      reduction = "cca", 
+                                      #k.anchor = 5,
+                                      dims = 1:50)
 
 rm(ref.list)
 
 # this command creates an 'integrated' data assay
-ref.combined <- IntegrateData(anchorset = ref.anchors, features.to.integrate = features.common)
+ref.combined <- IntegrateData(anchorset = ref.anchors, features.to.integrate = features.common, 
+                              dims = 1:50) ## take ~100G memory
 
 rm(ref.anchors)
+
 # specify that we will perform downstream analysis on the corrected data note that the
 # original unmodified data still resides in the 'RNA' assay
 DefaultAssay(ref.combined) <- "integrated"
 
-xx = DietSeurat(ref.combined, counts = TRUE, data = TRUE, scale.data = FALSE, assays = 'integrated')
+xx = DietSeurat(ref.combined, counts = TRUE, data = TRUE, scale.data = TRUE, assays = 'integrated')
 xx@assays$integrated@counts = ref.combined@assays$RNA@counts
 
-saveRDS(xx, file = paste0(RdataDir, 'Seurat.obj_adultMiceHeart_Forte2020_Ren2020_refCombined_logNormalize_counts_v3.rds'))
-
+#saveRDS(xx, file = paste0(outDir, 
+#                          '/Seurat.obj_mouseGastrulation_mNT_integrated.rds'))
 
 # Run the standard workflow for visualization and clustering
-ref.combined = readRDS(file =paste0(RdataDir, 
-                                    'Seurat.obj_adultMiceHeart_Forte2020_Ren2020_refCombined_logNormalize_counts_v3.rds'))
+ref.combined = readRDS(file =paste0(outDir, 
+                                    '/Seurat.obj_mouseGastrulation_mNT_integrated.rds'))
+
 
 ref.combined <- ScaleData(ref.combined, verbose = FALSE)
-ref.combined <- RunPCA(ref.combined, npcs = 30, verbose = FALSE)
+ref.combined <- RunPCA(ref.combined, npcs = 50, verbose = FALSE)
 
-ElbowPlot(ref.combined, ndims = 30)
+ElbowPlot(ref.combined, ndims = 50)
 
-ref.combined <- FindNeighbors(ref.combined, reduction = "pca", dims = 1:20)
-ref.combined <- FindClusters(ref.combined, resolution = 0.2)
+#ref.combined <- FindNeighbors(ref.combined, reduction = "pca", dims = 1:20)
+#ref.combined <- FindClusters(ref.combined, resolution = 0.2)
+ref.combined <- RunUMAP(ref.combined, reduction = "pca", dims = 1:50, n.neighbors = 50, 
+                        min.dist = 0.1) 
 
-ref.combined <- RunUMAP(ref.combined, reduction = "pca", dims = 1:30, n.neighbors = 50, min.dist = 0.05) 
+#DimPlot(ref.combined, reduction = "umap")
 
-DimPlot(ref.combined, reduction = "umap")
+kk = which(ref.combined$dataset == 'mNT') 
+ref.combined$celltype[kk] = ref.combined$condition[kk]
 
-kk = which(ref.combined$dataset == 'Ren2020' & ref.combined$celltype != 'CM')
-ref.combined$celltype[kk] = paste0(ref.combined$celltype[kk], '_Ren2020')
 
 # Visualization
-p1 <- DimPlot(ref.combined, reduction = "umap", group.by = "dataset")
+p1 <- DimPlot(ref.combined, reduction = "umap", group.by = "dataset", raster=FALSE)
 p2 <- DimPlot(ref.combined, reduction = "umap", group.by = "celltype", label = TRUE,
-              repel = TRUE)
-p1 + p2 
+              repel = TRUE, raster=FALSE) + NoLegend() 
 
-ggsave(paste0(resDir, '/Forte2020_Ren2020_IntegrationRPCA_', Normalization, '.pdf'), 
+p1 / p2 
+
+ggsave(paste0(outDir, '/Integration_dataset_celltypes.pdf'), 
+       width = 16, height = 24)
+
+DimPlot(ref.combined, reduction = "umap", group.by = "celltype", label = TRUE, split.by = 'dataset',
+        repel = TRUE, raster=FALSE) + NoLegend()
+
+ggsave(paste0(outDir, '/Integration_celltypes_split.dataset.pdf'), 
        width = 24, height = 10)
 
+DimPlot(ref.combined, reduction = "umap", group.by = "stage", label = TRUE,
+        repel = TRUE, raster=FALSE)
 
-
+ggsave(paste0(outDir, '/Integration_stage.pdf'), 
+       width = 16, height = 10)
 
